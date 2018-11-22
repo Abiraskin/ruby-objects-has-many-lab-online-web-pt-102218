4e365e839eb22a82aadb8c28df1f91c2011e2917
Abi Raskin <abiraskin@gmail.com>
2018-11-22 05:48:26 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,6 +9,19 @@ class Author
     @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def add_post(post)  
+    post.author = self  
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self  
+  end  
+    
   def posts
     Post.all.select {|p| p.auther == author}
   end
