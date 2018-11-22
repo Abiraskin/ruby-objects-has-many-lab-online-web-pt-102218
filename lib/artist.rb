0f441865c9c08b15e769d106729d0f35864f8222
Abi Raskin <abiraskin@gmail.com>
2018-11-22 05:10:14 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,22 @@
+class Artist
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def songs
+    @songs
+  end
+
+  def song_count
+    @songs.count
+  end
+
+end
